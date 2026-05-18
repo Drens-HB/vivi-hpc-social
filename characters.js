405ae6bb42dd3e78554b965828ce281540cdae2e
Drens-HB <frigopie90@gmail.com>
2026-05-19 00:06:41 +0200

    Update characters.js

--- characters.js
+++ characters.js
@@ -4,28 +4,55 @@ const characters = [
 
 {
     name:"Jur Klopp",
+
     subtitle:"Mejor amigo",
+
+    description:
+    "Una presencia extraña. Siempre parece observar más de lo que habla.",
+
     gems:7,
     max:10,
+
     image:"img/jur.png",
+
+    previewImage:"img/jur-preview.png",
+
     page:"personajes/jur.html"
 },
 
 {
     name:"Vivi Ornitier",
+
     subtitle:"Confianza extraña",
+
+    description:
+    "Hay algo profundamente frágil y peligroso en él.",
+
     gems:5,
     max:10,
+
     image:"img/vivi.png",
+
+    previewImage:"img/vivi-preview.png",
+
     page:"personajes/vivi.html"
 },
 
 {
     name:"Ramsay",
+
     subtitle:"Relación tensa",
+
+    description:
+    "La cicatriz en su rostro parece menos peligrosa que su mirada.",
+
     gems:2,
     max:10,
+
     image:"img/ramsay.png",
+
+    previewImage:"img/ramsay-preview.png",
+
     page:"personajes/ramsay.html"
 }
 
