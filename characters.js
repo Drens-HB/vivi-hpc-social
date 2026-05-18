790de173a7d4d214d85432ff8a9f3aabcbfaca68
Drens-HB <frigopie90@gmail.com>
2026-05-19 00:01:03 +0200

    Create characters.js

---
+++ characters.js
@@ -0,0 +1,32 @@
+// characters.js
+
+const characters = [
+
+{
+    name:"Jur Klopp",
+    subtitle:"Mejor amigo",
+    gems:7,
+    max:10,
+    image:"img/jur.png",
+    page:"personajes/jur.html"
+},
+
+{
+    name:"Vivi Ornitier",
+    subtitle:"Confianza extraña",
+    gems:5,
+    max:10,
+    image:"img/vivi.png",
+    page:"personajes/vivi.html"
+},
+
+{
+    name:"Ramsay",
+    subtitle:"Relación tensa",
+    gems:2,
+    max:10,
+    image:"img/ramsay.png",
+    page:"personajes/ramsay.html"
+}
+
+];
